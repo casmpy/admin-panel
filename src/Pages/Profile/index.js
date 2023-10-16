35a0f1a73620778e8db872461461ce7161b51269
Navjot singh <64366626+casmpy@users.noreply.github.com>
2023-10-17 00:22:23 +0530

    main

---
+++ src/Pages/Profile/index.js
@@ -0,0 +1,14 @@
+import HeaderSection from "@aio/components/HeaderSection";
+
+const Profile = (props) => {
+    return (
+        <>
+            <HeaderSection 
+                heading={'Hello'}
+                subHeading={'Lets check your stats today!'}
+            />
+        </>
+    );
+}
+
+export default Profile;
